1feacab206a5635e95d4b7e811d4e464b279cbec
ashrafmohammad7 <ashrafmohammed07557@gmail.com>
2026-05-24 02:25:33 +0530

    Fixed render session persistence

--- app.js
+++ app.js
@@ -61,11 +61,11 @@ const sessionOptions = {
     saveUninitialized: false,
     proxy: true,
     cookie: {
-        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: false,
         sameSite: "lax",
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     },
 };
 
